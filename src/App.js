d45bed2ec3c1e596d43b209a2dd5ec2d0b47072c
Pavel Slipko <pavel20042019@mail.ru>
2022-02-08 21:48:39 +0600

    asd

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>Начало сайта по респираторным маскам</h1>
-<<<<<<< HEAD
+
         <h3>Lorem ipsum dolor sit amet.</h3>
-=======
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
->>>>>>> 17c4c44db083b60e752142c93c9e8518bf735327
+
     </div>
   );
 }
