6990e3d3ec844c8b05572605f8d211bfaada6010
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-02-08 22:12:15 +0600

    homeicons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Начало сайта по респираторным маскам</h1>
-            <h3>Lorem ipsum dolor sit amet.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
-        </div>
   return (
+    <div className="App">
+
+      <h1>Начало сайта по респираторным маскам</h1>
+
+        <h3>Lorem ipsum dolor sit amet.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
+
+    </div>
   );
 }
 
