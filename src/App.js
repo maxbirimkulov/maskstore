3d5ab36bc332c3e49203c1b0d2b9f80b4108b97d
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-02-08 21:51:26 +0600

    stash

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Начало сайта по респираторным маскам</h1>
-<<<<<<< HEAD
-        <h3>Lorem ipsum dolor sit amet.</h3>
-=======
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
->>>>>>> 17c4c44db083b60e752142c93c9e8518bf735327
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Начало сайта по респираторным маскам</h1>
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
+        </div>
+    );
 }
 
 export default App;
