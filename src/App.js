8764aa6f8e0a9b0f0fc81f02b97f21117a4a28ae
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-02-10 22:00:25 +0600

    fix opt

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ import Opt from "./pages/Home/Opt/Opt";
 function App() {
     return (
         <div className="App">
-            <h1>Начало сайта по респираторным маскам</h1>
-            <h3>Lorem ipsum dolor sit amet.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
-            <Opt/>
+            <Home/>
             <Footer/>
         </div>
     );
