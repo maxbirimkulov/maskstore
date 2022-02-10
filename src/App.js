453bba181a8d5b5dde0cb8f91eda261c6046ae84
Sagynov Ryskul <sagynovryskul.ru>
2022-02-10 22:12:42 +0600

    ryskul

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Footer from "./components/Footer/Footer";
-
+import Catalog from './components/Catalog/Catalog'
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-
       <h1>Начало сайта по респираторным маскам</h1>
 
         <h3>Lorem ipsum dolor sit amet.</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
+      <Catalog/>
         <Footer/>
     </div>
   );
