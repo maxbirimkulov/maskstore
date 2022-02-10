44b569e1e407d3c00d57ccc1208cc1adde032b27
ACER <a.doroev@mail.ru>
2022-02-10 22:04:59 +0600

    fix2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react'
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
-<<<<<<< HEAD
-import './style.css'
-=======
 import './style.css';
->>>>>>> 88e8db268f7e8ee7d13b70c9dcaeec9496dc9a67
-import Opt from "./pages/Home/Opt/Opt";
 
 
 function App() {
-<<<<<<< HEAD
   return (
     <div className="App">
       <h1>Начало сайта по респираторным маскам</h1>
         <h3>Lorem ipsum dolor sit amet.</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
-      <Opt/>
         <Footer/>
     </div>
   );
-=======
     return (
         <div className="App">
             <h1>Начало сайта по респираторным маскам</h1>
@@ -30,7 +22,6 @@ function App() {
             <Footer/>
         </div>
     );
->>>>>>> 88e8db268f7e8ee7d13b70c9dcaeec9496dc9a67
 }
 
 export default App;
