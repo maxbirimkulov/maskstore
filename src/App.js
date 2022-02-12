3f74040775761f79319fc0c66e664388b6c9d979
Sagynov Ryskul <sagynovryskul.ru>
2022-02-12 20:38:13 +0600

    chenge App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
-<<<<<<< HEAD
 import Catalog from './components/Catalog/Catalog'
-import './style.css'
-=======
-<<<<<<< HEAD
-import './style.css';
-
-=======
-<<<<<<< HEAD
 import Messages from './pages/Home/Messages/Messages'
 import "./style.css"
 
->>>>>>> c3594cb0ba51e7a42cc75959f823bc1259215696
 
 function App() {
   return (
@@ -22,37 +13,13 @@ function App() {
 
         <h3>Lorem ipsum dolor sit amet.</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
-<<<<<<< HEAD
       <Catalog/>
-=======
-
         <Messages/>
->>>>>>> c3594cb0ba51e7a42cc75959f823bc1259215696
         <Footer/>
     </div>
   );
-=======
-import Header from "./components/Header/Header";
-import "./style.css";
-import Opt from "./pages/Home/Opt/Opt";
->>>>>>> 2fc94da9d14e20fb967096d87ae65131de5fa7d3
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Начало сайта по респираторным маскам</h1>
-        <h3>Lorem ipsum dolor sit amet.</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, nobis.</p>
-        <Footer/>
-    </div>
-  );
-    return (
-        <div className="App">
-            <Home/>
-            <Footer/>
-        </div>
-    );
->>>>>>> b5cd8ddb672b3a31388bc69fb6bce1666ece3655
+
 }
 
 export default App;
