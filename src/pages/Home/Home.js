2c88d4237d481009388435b9b74e22684a10fbe9
Sagynov Ryskul <sagynovryskul.ru>
2022-02-12 20:50:21 +0600

    chenge

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import Footer from "../../components/Footer/Footer";
 import Catalog from '../../components/Catalog/Catalog'
-const Home = () => {
-    return (
-        <div>
-                <Catalog/>
-        </div>
-=======
 import FirstScreen from './FirstScreen/FirstScreen';
 import Opt from './Opt/Opt';
 
@@ -17,7 +10,6 @@ const Home = () => {
             <FirstScreen/>
             <Opt/>
         </main>
->>>>>>> c3594cb0ba51e7a42cc75959f823bc1259215696
     );
 };
 
