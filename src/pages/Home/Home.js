35cb5f6a82fa82a44d089793a5cfcdae3dfc55f9
Maksat Birimkulov <maxbirimkulov@mail.ru>
2022-02-12 20:43:37 +0600

    asd

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import FirstScreen from './FirstScreen/FirstScreen';
+import Catalog from "../../components/Catalog/Catalog";
+import AboutUs from "./AboutUs/AboutUs";
+import Info from "./Info/Info";
+import Opt from "./Opt/Opt";
+import Messages from "./Messages/Messages";
+import Express from "./Express/Express";
 
 const Home = () => {
     return (
         <main>
             <FirstScreen/>
+            <Catalog/>
+            <AboutUs/>
+            <Info/>
+            <Opt/>
+            <Messages/>
+            <Express/>
         </main>
     );
 };
